1b7efdd4755e1678fa24612cb9054a8719cfe4ae
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-09-25 21:07:47 +0300

    Added search-filter with beginforms to Products Index -View

--- WebAppFirst/Controllers/ProductsController.cs
+++ WebAppFirst/Controllers/ProductsController.cs
@@ -10,19 +10,31 @@ namespace WebAppFirst.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string currentFilter1, string searchString1)
         {
-            if (Session["UserName"] == null)
-            {
-                return RedirectToAction("login", "home");
-            }
-            else
-            {
+            //if (Session["UserName"] == null)
+            //{
+            //    return RedirectToAction("login", "home");
+            //}
+            //else
+            //{
                 northwindEntities db = new northwindEntities();
-                List<Products> model = db.Products.ToList();
+
+                // tähän tuotteet-olioon voidaan myöhemmin kohdistaa lisää metodeja
+                var tuotteet = from p in db.Products
+                               select p;
+
+                // eli jos searchString ei ole tyhjä, sitten tehdään rajaus Where tuotteen nimi sisältää käyttäjän antaman tekstin (searchString1). Sisältää kaikki, joissa kirjoitettu sisältyy koska "contains"
+                if (!String.IsNullOrEmpty(searchString1))
+                {
+                    tuotteet = tuotteet.Where(p => p.ProductName.Contains(searchString1));
+                }
+
+                // Vanhat hakulauseet:
+                //List<Products> model = db.Products.ToList();
                 //db.Dispose();
-                return View(model);
-            }
+                return View(tuotteet);
+            //}
         }
 
         public ActionResult Index2()
